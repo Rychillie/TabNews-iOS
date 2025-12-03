297d156d50a28bf9880c6520f917df87f9822ae1
Rychillie <rychillie@rychillie.net>
2025-12-02 23:11:43 -0300

    Initial Commit

---
+++ TabNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabNews
+//
+//  Created by Rychillie Umpierre de Oliveira on 02/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabNews/TabNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabNewsApp.swift
+//  TabNews
+//
+//  Created by Rychillie Umpierre de Oliveira on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TabNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
